45150b2c23a14731e5571d05456fdd47bd0d29c9
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2024-12-08 21:04:01 +0700

    Added basic

---
+++ pepe.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+import argparse
+from sys import argv
+from data.settings import *
+from services.config import *
+from services.actions import ActionsList, actions
+from services.args import ProgramArgs
+
+def execute_args(args: ProgramArgs):
+    action = args.action
+    match action.lower():
+        case ActionsList.LIST:
+            actions[ActionsList.LIST]()
+
+def main():
+    # parse args
+    parser = argparse.ArgumentParser(
+        prog=f'{PROG_NAME} {PROG_VERSION}',
+        description=PROG_DESCRIPTION
+    )
+    parser.add_argument('action')
+    execute_args(parser.parse_args())
+
+create_config()
+main()
