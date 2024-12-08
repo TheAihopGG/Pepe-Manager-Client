46aeb3031b781edae8b68c61e9403d0853e3e11e
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2024-12-08 21:03:34 +0700

    Added `LOGS_PATH` (not working at the moment) `PROG_NAME` `PROG_VERSION` `PROG_DESCRIPTION`

--- data/settings.py
+++ data/settings.py
@@ -1,2 +1,8 @@
-# Paths
+# paths
 CONFIG_PATH = 'data/config.json'
+LOGS_PATH = 'logs.log'
+
+# names
+PROG_NAME = 'Pepe Manager'
+PROG_VERSION = '1.0.0'
+PROG_DESCRIPTION = 'Pepe manager is a program, it is like an apt, pacman. This project to practice python and requests.'
