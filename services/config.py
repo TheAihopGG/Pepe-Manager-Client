a5f4a44e42863cf268bf586f67a1e1292ad06de2
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2024-12-08 22:18:21 +0700

    Added `init_config` (renamed) `load_config` `edit_config`

--- services/config.py
+++ services/config.py
@@ -1,7 +1,7 @@
 import rich
 import json
 import functools
-from typing import TypedDict, Callable
+from typing import TypedDict, Callable, Any
 from os.path import exists
 from data.settings import CONFIG_PATH
 
@@ -14,29 +14,21 @@ class TypedConfig(TypedDict):
     packages_dir_path: str
     packages: list[dict]
 
-def create_config(config_path: str = CONFIG_PATH):
+
+def init_config(config_path: str = CONFIG_PATH):
     if not exists(config_path):
         with open(config_path, 'w') as config_file:
             json.dump(config_file, DEFAULT_CONFIG)
 
+
 def load_config(config_path: str = CONFIG_PATH) -> TypedConfig:
-    create_config(config_path=config_path)
+    init_config(config_path=config_path)
     return json.load(open(config_path))
 
-def reload_config_deco(func: Callable[..., None], **kwargs):
-    """Reload config in global var `config`"""
-    rich.print(f'invoked: {reload_config_deco.__name__}')
-    @functools.wraps(func)
-    def wrapper():
-        global config
-        config = load_config()
-        return func(**kwargs)
-    return wrapper
 
-def reload_config():
-    """Reload config in global var `config`"""
-    global config
-    config = load_config()
-    rich.print(f'invoked: {__name__}')
-
-config = load_config()
+def edit_config(key: str, value: Any):
+    with open(CONFIG_PATH, 'r') as config_file:
+        config: TypedConfig = json.load(config_file)
+    config[key] = value
+    with open(CONFIG_PATH, 'w') as config_file:
+        json.dump(config, config_file)
